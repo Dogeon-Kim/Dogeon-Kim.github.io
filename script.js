8fa47dd39f75363f03c6637dd8607b9245636443
DogeonKim <73727010+Dogeon-Kim@users.noreply.github.com>
2022-07-10 22:09:20 +0900

    Change Code and Commit.

---
+++ script.js
@@ -0,0 +1,64 @@
+const canvas = document.getElementById('side');
+        const ctx = canvas.getContext("2d");
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(355, 105);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(1200, 0);
+        ctx.lineTo(855, 105);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(0, 700);
+        ctx.lineTo(355, 405);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(1200, 700);
+        ctx.lineTo(855, 405);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+
+        ctx.beginPath();
+        ctx.moveTo(563, 500);
+        ctx.lineTo(588, 250);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(647, 500);
+        ctx.lineTo(622, 250);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+        
+        ctx.beginPath();
+        ctx.moveTo(588, 250);
+        ctx.lineTo(622, 250);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+
+        ctx.beginPath();
+        ctx.moveTo(350, 405);
+        ctx.lineTo(570, 405);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(855, 405);
+        ctx.lineTo(640, 405);
+        ctx.lineWidth = 5;
+        ctx.stroke();
+
+        /*book*/
+        ctx.beginPath();
+        ctx.moveTo(225, 360);
+        ctx.lineTo(640, 405);
+        ctx.lineWidth = 5;
+        ctx.stroke();
